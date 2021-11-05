e5d4b4d4ff79c52b63c6d62cb3356ec3caa2d57d
Yohan Moon <yohan.moon@outlook.com>
2021-11-05 00:07:09 -0400

    added more css

---
+++ submit.js
@@ -0,0 +1,22 @@
+function getInputValue() {
+    
+   
+
+    let a = document.getElementById("FirstNameInput").value;
+    let b= document.getElementById("LastNameInput").value;
+    let c=document.getElementById("EmailInput").value;
+    let d=document.getElementById("CompanyInput").value;
+    let e= document.getElementById("RoleInput").value;
+    let f=document.getElementById("CountryInput").value;
+    let g=document.getElementById("LanguageInput").value;
+    
+        alert("First Name : "+ a+ 
+    "\n Last Name : " +b +
+    "\n Email : " +c +
+    "\n Compnay : " +d +
+    "\n Role : " +e+
+    "\n Country : " +f+
+    "\n Language : " +g );
+    
+
+}
